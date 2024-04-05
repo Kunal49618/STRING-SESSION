34c51fa02c2a5181894d8b645d9ad149eaabcf06
Kunal49618 <159246164+Kunal49618@users.noreply.github.com>
2024-04-05 10:16:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ load_dotenv()
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("OWNER_ID"))
+BOT_TOKEN = getenv("7158227205:AAGSk3bpEyLGxcQASpZxpqQs_LCC6VF9l6U")
+OWNER_ID = int(getenv("7173620352"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", None)
+MONGO_DB_URI = getenv("mongodb+srv://BrandedSupportGroup:BRANDED_WORLD@cluster0.v4odcq9.mongodb.net/?retryWrites=true&w=majority")
+MUST_JOIN = getenv("https://t.me/KNOW_ABOUT_KUNAL", None)
